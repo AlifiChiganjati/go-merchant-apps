1933381dff735410e908ad6db4c58b26b426ea33
Alifi Chiganjati <alifichiganjati@gmail.com>
2026-02-26 15:05:07 +0700

    FEAT: sql scripts

--- assets/db/db.sql
+++ assets/db/db.sql
@@ -1,70 +1,63 @@
-CREATE DATABASE IF NOT EXISTS db_sms_ppob;
+CREATE DATABASE db_merchant;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS "users"(
+CREATE TABLE IF NOT EXISTS "users" (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  email VARCHAR(255) NOT NULL UNIQUE,
-  first_name VARCHAR(255) NOT NULL, 
-  last_name VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  profile_image VARCHAR(255),
-  saldo DECIMAL(10,2) DEFAULT 0,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  fullname VARCHAR(225) NOT NULL,
+  password VARCHAR(225) NOT NULL,
+  email VARCHAR(225) UNIQUE NOT NULL,
+  phone_number VARCHAR(15) NOT NULL,
+  role VARCHAR(255) NOT NULL,
+  point INTEGER DEFAULT 0 NOT NULL,
+  created_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS "banners"(
+CREATE TABLE IF NOT EXISTS "merchants" (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  banner_name VARCHAR(255),
-  banner_image VARCHAR(255),
-  description VARCHAR(255),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  user_id UUID  REFERENCES users(id),
+  merchant_name VARCHAR(30) NOT NULL,
+  merchant_address VARCHAR(100) NOT NULL,
+  created_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS "services"(
+CREATE TABLE IF NOT EXISTS "products" (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  service_code VARCHAR(255),
-  service_name VARCHAR(255),
-  service_icon VARCHAR(255),
-  service_tarif DECIMAL(10,2),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  merchant_id UUID  REFERENCES merchants(id),
+  name VARCHAR(30) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  point INTEGER NOT NULL,
+  created_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS "transactions"(
+CREATE TABLE IF NOT EXISTS "cart"(
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  user_id UUID REFERENCES users(id),
-  service_id UUID REFERENCES services(id),
-  transaction_type VARCHAR(255) NOT NULL,
-  invoice_number VARCHAR(255) NOT NULL,
-  total_amount DECIMAL(10,2) NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  user_id UUID  REFERENCES users(id),
+  product_id UUID REFERENCES products(id),
+  quantity INTEGER NOT NULL,
+  created_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO "banners" 
-  (banner_name,banner_image,description,created_at,updated_at)
-VALUES
-  ('Banner 1','https://placehold.co/600x400','Lorem Ipsum Dolor sit amet',current_timestamp,current_timestamp),
-  ('Banner 2','https://placehold.co/600x400','Lorem Ipsum Dolor sit amet',current_timestamp,current_timestamp),
-  ('Banner 3','https://placehold.co/600x400','Lorem Ipsum Dolor sit amet',current_timestamp,current_timestamp),
-  ('Banner 4','https://placehold.co/600x400','Lorem Ipsum Dolor sit amet',current_timestamp,current_timestamp),
-  ('Banner 5','https://placehold.co/600x400','Lorem Ipsum Dolor sit amet',current_timestamp,current_timestamp),
-  ('Banner 6','https://placehold.co/600x400','Lorem Ipsum Dolor sit amet',current_timestamp,current_timestamp);
+CREATE TABLE IF NOT EXISTS "transactions" (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id UUID  REFERENCES users(id),
+  merchant_id UUID  REFERENCES merchants(id),
+  transactions_no INTEGER NOT NULL,
+  point_total INTEGER NOT NULL,
+  subtotal DECIMAL(10,2) NOT NULL
+);
 
-INSERT INTO "services"
-  (service_code, service_name, service_icon, service_tarif,created_at,updated_at)
-VALUES 
-  ('PAJAK','Pajak PBB','https://placehold.co/600x400',40000,current_timestamp,current_timestamp),
-  ('PLN','Listrik','https://placehold.co/600x400',10000,current_timestamp,current_timestamp),
-  ('PDAM','PDAM Berlangganan','https://placehold.co/600x400',40000,current_timestamp,current_timestamp),
-  ('PULSA','Pulsa','https://placehold.co/600x400',40000,current_timestamp,current_timestamp),
-  ('PGN','PGN Berlangganan','https://placehold.co/600x400',50000,current_timestamp,current_timestamp),
-  ('MUSIK','Musik Berlangganan','https://placehold.co/600x400',50000,current_timestamp,current_timestamp),
-  ('TV','TV Berlangganan','https://placehold.co/600x400',50000,current_timestamp,current_timestamp),
-  ('PAKET_DATA','Paket Data','https://placehold.co/600x400',50000,current_timestamp,current_timestamp),
-  ('VOUCHER_GAME','Voucher Game','https://placehold.co/600x400',100000,current_timestamp,current_timestamp),
-  ('VOUCHER_MAKANAN','Voucher Makanan','https://placehold.co/600x400',100000,current_timestamp,current_timestamp),
-  ('QURBAN','Qurban','https://placehold.co/600x400',200000,current_timestamp,current_timestamp),
-  ('ZAKAT','Zakat','https://placehold.co/600x400',300000,current_timestamp,current_timestamp);
+CREATE TABLE IF NOT EXISTS "transactions_detail" (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  transaction_id UUID  REFERENCES transactions(id),
+  product_id UUID  REFERENCES products(id),
+  qty INTEGER NOT NULL,
+  point INTEGER NOT NULL,
+  total DECIMAL(10,2) NOT NULL,
+  created_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
+);
